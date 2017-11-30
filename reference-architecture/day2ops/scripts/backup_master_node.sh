fb511fb56f35316377834a6c3f6a7c58103eb8b4
Eduardo Minguez <edu@redhat.com>
2017-11-30 15:23:33 +0100

    Reworked to be used with masters and nodes

--- reference-architecture/day2ops/scripts/backup_master_node.sh
+++ reference-architecture/day2ops/scripts/backup_master_node.sh
@@ -15,19 +15,30 @@ usage(){
 }
 
 ocpfiles(){
-  mkdir -p ${BACKUPLOCATION}/sysconfig
+  mkdir -p ${BACKUPLOCATION}/etc/sysconfig
   echo "Exporting OCP related files to ${BACKUPLOCATION}"
-  cp -aR /etc/origin ${BACKUPLOCATION}
-  cp -aR /etc/sysconfig/atomic-* ${BACKUPLOCATION}/sysconfig
+  cp -aR /etc/origin ${BACKUPLOCATION}/etc
+  cp -aR /etc/sysconfig/atomic-* ${BACKUPLOCATION}/etc/sysconfig
 }
 
 otherfiles(){
-  mkdir -p ${BACKUPLOCATION}/{sysconfig,external_certificates}
+  mkdir -p ${BACKUPLOCATION}/etc/sysconfig
+  mkdir -p ${BACKUPLOCATION}/etc/pki/ca-trust/source/anchors
   echo "Exporting other important files to ${BACKUPLOCATION}"
-  cp -aR /etc/sysconfig/{iptables,docker-*,flanneld} \
-    ${BACKUPLOCATION}/sysconfig/
+  if [ -f /etc/sysconfig/flanneld ]
+  then
+    cp -a /etc/sysconfig/flanneld \
+      ${BACKUPLOCATION}/etc/sysconfig/
+  fi
+  cp -aR /etc/sysconfig/{iptables,docker-*} \
+    ${BACKUPLOCATION}/etc/sysconfig/
+  if [ -d /etc/cni ]
+  then
+    cp -aR /etc/cni ${BACKUPLOCATION}/etc/
+  fi
+  cp -aR /etc/dnsmasq* ${BACKUPLOCATION}/etc/
   cp -aR /etc/pki/ca-trust/source/anchors/* \
-    ${BACKUPLOCATION}/external_certificates
+    ${BACKUPLOCATION}/etc/pki/ca-trust/source/anchors/
 }
 
 packagelist(){

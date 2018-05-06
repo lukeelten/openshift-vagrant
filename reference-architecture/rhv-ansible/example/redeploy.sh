2f6df163b95973ee650cd2314d04c67ed677bea1
Chandler Wilkerson <cwilkers@redhat.com>
2018-05-06 16:27:36 +0000

    simplified redeploy script

--- reference-architecture/rhv-ansible/example/redeploy.sh
+++ reference-architecture/rhv-ansible/example/redeploy.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
-VARIANT=${1:-centos}
-if [ -f "test/ocp-vars.yaml.$VARIANT" ]
-then
-    VARS="-e@test/ocp-vars.yaml.$VARIANT"
-else
-    VARS="-e@test/ocp-vars.yaml"
-fi
+VARIANT=${1:-39}
 
-if [ -f "test/ovirt-${VARIANT}-infra.yaml" ]
+if [ -f "example/ovirt-${VARIANT}-infra.yaml" ]
 then
-    INFRA="test/ovirt-${VARIANT}-infra.yaml"
+    INFRA="example/ovirt-${VARIANT}-infra.yaml"
 else
     INFRA="playbooks/ovirt-vm-infra.yaml"
 fi
 
-ansible-playbook $VARS test/uninstall.yaml 
-ansible-playbook $VARS $INFRA
+ansible-playbook -e@~/vault.yaml example/uninstall.yaml 
+ansible-playbook -e@~/vault.yaml $INFRA
 if [ "$?" != "0" ]; then
   echo "Infrastructure deploy broke"
   exit
 fi
-ansible-playbook $VARS playbooks/output-dns.yaml 
+echo "Waiting a minute for VMs to get IPs posted"
+sleep 60
+ansible-playbook -i /etc/ansible/hosts -i inventory playbooks/output-dns.yaml 
 if [ "$?" != "0" ]; then
-  echo "DNS generation broke"
-  exit
+  echo "DNS generation broke. Backing off and retrying in two minutes"
+  sleep 120
+  ansible-playbook -i /etc/ansible/hosts -i inventory playbooks/output-dns.yaml 
+  if [ "$?" != "0" ]; then
+    echo "Still breaking DNS generation. exiting."
+    exit
+  fi
 fi
 nsupdate -k /etc/rndc.key inventory.nsupdate 
 if [ "$?" != "0" ]; then
   echo "DNS update broke"
   exit
 fi
-ansible-playbook $VARS playbooks/openshift-install.yaml 
+ansible-playbook -e@~/vault.yaml /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
+if [ "$?" != "0" ]; then
+  echo "Prerequisites installation broke"
+  exit
+fi
+ansible-playbook -e@~/vault.yaml /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
 
